c83c92aab1f835d91d341328ad62815021a09e56
AvirajDB <bawkaraviraj@gmail.com>
2026-04-21 12:26:17 +0530

    Push-21/4

--- src/componets/Home.jsx
+++ src/componets/Home.jsx
@@ -9,8 +9,20 @@ const Home = () => {
       <div className='hero-img'>
         <img src={hero} alt=""  className='img'/>
         <div className='home-comp1'>
-           <h1 className='scroll'>SCROLL</h1>
-           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi repellendus et reprehenderit accusantium aspernatur at placeat aperiam, consectetur est sint quidem officia molestias. Cum sequi harum facilis nisi nam ut.</p>
+           <h4 className='scroll'>SCROLL</h4>
+           <p className='home-comp1-para'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi repellendus et reprehenderit accusantium aspernatur at placeat aperiam, consectetur est sint quidem officia molestias. Cum sequi harum facilis nisi nam ut.</p>
+          <div className='home-com2'>
+               <h2>Players</h2>
+               <div className='home-playerslider'>
+                <button>p</button>
+                <button>N</button>
+                <button className='enter'>Enter</button>
+               </div>
+          </div>
+        </div>
+        <div className='home-comp3'>
+           
+
         </div>
 
       </div>
